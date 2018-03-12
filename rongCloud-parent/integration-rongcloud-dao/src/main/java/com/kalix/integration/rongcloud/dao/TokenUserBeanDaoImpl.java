a7ed5483e0a3430e8c9081dc594dbb9acd905587
fengweibin <tx_xinxin2006@163.com>
2018-03-12 10:12:17 +0800

    修改包名及文件名

---
+++ rongCloud-parent/integration-rongcloud-dao/src/main/java/com/kalix/integration/rongcloud/dao/TokenUserBeanDaoImpl.java
@@ -0,0 +1,14 @@
+package com.kalix.integration.rongcloud.dao;
+
+import com.kalix.integration.rongcloud.api.dao.ITokenUserBeanDao;
+import com.kalix.integration.rongcloud.entities.TokenUserBean;
+
+/**
+ * 融云用户DAO实现
+ * @author majian <br/>
+ *         date:2015-7-23
+ * @version 1.0.0
+ */
+public class TokenUserBeanDaoImpl extends BaseAdminDao<TokenUserBean, Long> implements ITokenUserBeanDao {
+
+}
