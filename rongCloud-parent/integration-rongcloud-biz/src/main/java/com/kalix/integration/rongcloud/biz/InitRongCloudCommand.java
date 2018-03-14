702ea940e18f2fe88fcc71ab50748258ae8c15d2
fengweibin <tx_xinxin2006@163.com>
2018-03-14 15:08:08 +0800

    增加默认头像路径处理

--- rongCloud-parent/integration-rongcloud-biz/src/main/java/com/kalix/integration/rongcloud/biz/InitRongCloudCommand.java
+++ rongCloud-parent/integration-rongcloud-biz/src/main/java/com/kalix/integration/rongcloud/biz/InitRongCloudCommand.java
@@ -1,4 +1,6 @@
 package com.kalix.integration.rongcloud.biz;
+import com.kalix.framework.core.util.ConfigUtil;
+import com.kalix.framework.core.util.StringUtils;
 import com.kalix.integration.rongcloud.rong.io.rong.RongCloud;
 import com.kalix.integration.rongcloud.rong.io.rong.models.TokenResult;
 import org.apache.karaf.shell.api.action.Argument;
@@ -21,9 +23,7 @@ import java.sql.Statement;
 public class InitRongCloudCommand implements Action {
     @Argument(index = 0, name = "arg", description = "The command argument", required = false, multiValued = false)
     @Completion(MyCompleter.class)
-   private String appKey = "kj7swf8okidb2";//替换成您的appkey
-   private String appSecret = "xCcyoNpqIh";//替换成匹配上面key的secret
-
+    private static String CONFIG_FILE_NAME = "ConfigWebContext";
     @Override
     public Object execute() throws Exception {
         init();
@@ -32,28 +32,41 @@ public class InitRongCloudCommand implements Action {
 
     private void init() {
         DataSource dataSource = Util.getKalixDataSource();
+        String appKey = (String) ConfigUtil.getConfigProp("appKey", CONFIG_FILE_NAME);
+        String appSecret = (String) ConfigUtil.getConfigProp("appSecret", CONFIG_FILE_NAME);
         RongCloud rongCloud = RongCloud.getInstance(appKey, appSecret);
+
+        String serverUrl = (String) ConfigUtil.getConfigProp("server_url", CONFIG_FILE_NAME);
         String sql="";
         Statement statement=null;
         Connection conn=null;
         ResultSet rs=null;
+        String userHeaderImg="";
         // 获取 Token 方法
         TokenResult userGetTokenResult = null;
         try {
              conn = dataSource.getConnection();
              statement = conn.createStatement();
+             statement.execute("DELETE from public.sys_token_user");
              conn.setAutoCommit(false);
              rs=statement.executeQuery("SELECT * from SYS_USER order by id ");
             // int i=0;
             while (rs.next())
             {
-                //频繁调用融云接口会被拒绝，延迟五秒
-                Thread.currentThread().sleep(5000);//毫秒
+                //频繁调用融云接口会被拒绝，延迟2秒
+                Thread.currentThread().sleep(2000);//毫秒
               long userid= rs.getInt("id");
               System.out.println(userid);
               String username= rs.getString("name");
               String userImage=rs.getString("icon");
-              userGetTokenResult = rongCloud.user.getToken(String.valueOf(userid), username, "http://localhost:5984/kalix/332d506754c04760adf3646b6312d394/S206A-239+853110-239+855770.jpg");
+              if(StringUtils.isNotEmpty(userImage))
+              {
+                  userHeaderImg=userImage;
+              }else
+              {
+                  userHeaderImg=serverUrl+"/static/images/default_user.png";
+              }
+              userGetTokenResult = rongCloud.user.getToken(String.valueOf(userid), username, userHeaderImg);
               sql="INSERT INTO public.sys_token_user (id, createby, creationdate, updateby, updatedate,version_,token,userid) " +
                       "                               VALUES ('"+userid+"',null,null,null,null,null,'"+userGetTokenResult.getToken()+"','"+userid+"')";
                 statement.addBatch(sql);
@@ -102,20 +115,6 @@ public class InitRongCloudCommand implements Action {
         Util.outPrint("脚本执行成功！");
     }
 
-    /**
-     * clear data of db
-     */
-    private void beforeClearData() {
-//        StringReader reader = new StringReader(appClearSql + funClearSql + role_funClearSql + app_funClearSql);
-//        try {
-//            Util.outPrint("clean application data!");
-//            scriptRunner.runScript(reader);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-    }
 
     public static void main(String[] args) throws Exception {
         InitRongCloudCommand n =new InitRongCloudCommand();
